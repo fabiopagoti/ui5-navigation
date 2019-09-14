9076e0864323c3ca9a76babf0f9f528b22e6f535
Fábio Luiz Esperati Pagoti <fabiopagoti@gmail.com>
2019-09-14 01:22:16 +0000

    versao com SplitApp

---
+++ webapp/controller/S1.controller.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ovly.navigation.controller.S1", {
+
+		onInit: function () {
+
+		},
+		
+		onItemPress: function(oEvent){
+			
+			var oParameters = oEvent.getParameters();
+			var oListItem = oParameters.listItem; // NAO EH FUNCAO
+			var oContext = oListItem.getBindingContext();
+			var sId = oContext.getProperty("Team_Identifier"); 
+			
+			this.getOwnerComponent().getRouter().navTo("equipe", { 
+				idEquipe: sId
+			});
+		}
+
+	
+
+	});
+
+});
---
+++ webapp/controller/S2.controller.js
@@ -0,0 +1,34 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ovly.navigation.controller.S2", {
+
+		onInit: function () {
+			this.getOwnerComponent().getRouter().getRoute("equipe").attachPatternMatched(this.onPatternMatch, this);
+		},
+
+		onPatternMatch: function (oEvent) {
+			var oParameters = oEvent.getParameters();
+			var oArguments = oParameters.arguments; // NAO EH FUNCAO
+			var sIdEquipe = oArguments.idEquipe;
+
+			// console.log(sIdEquipe);
+			
+			this.getView().bindElement({
+				path: "/Teams('" + sIdEquipe + "')" 
+			});
+		},
+
+		onBack: function (oEvent) {
+			this.getOwnerComponent().getRouter().navTo("inicial");
+		},
+
+		onItem: function (oEvent) {
+
+		}
+
+	});
+
+});
